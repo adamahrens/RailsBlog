8b99c58400434ce09cad1d918f201f6f5673dc66
Adam Ahrens <adam.aahrens@gmail.com>
2019-09-07 14:19:37 -0500

    Adding validation around title and description property for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,4 +10,6 @@
 #
 
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -12,7 +12,31 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'model is not valid when title is missing' do
+    article = Article.new(description: 'Description')
+    assert_not article.valid?
+  end
+
+  test 'model is not valid when description is missing' do
+    article = Article.new(title: 'My Title')
+    assert_not article.valid?
+  end
+
+  test 'title must have a minimum of 5 characters' do
+    article = Article.new(description: 'Description')
+    article.title = 'a' * 4
+    assert_not article.valid?
+
+    article.title = 'a' * 5
+    assert article.valid?
+  end
+
+  test 'description must have a minimum of 5 characters' do
+    article = Article.new(title: 'My Title')
+    article.description = 'a' * 4
+    assert_not article.valid?
+
+    article.description = 'a' * 5
+    assert article.valid?
+  end
 end
