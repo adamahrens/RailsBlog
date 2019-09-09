1ece074a7fd68ba8586d1bf63eec590c4afb32a6
Adam Ahrens <adam.aahrens@gmail.com>
2019-09-09 15:13:36 -0500

    Adding uniqueness requirement to username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,11 @@
 #
 
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 5 }
+  validates :username, presence: true,
+                       length: { minimum: 5 },
+                       uniqueness: { case_sensitive: false }
   validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP,
                               message: 'must be a valid email' }
 end
