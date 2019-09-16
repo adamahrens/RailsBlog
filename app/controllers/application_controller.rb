ca350d50b586a2bddcde3fa59b63a4dafda83715
Adam Ahrens <adam.aahrens@gmail.com>
2019-09-15 21:25:40 -0500

    Requiring a logged in user for Articles except on show and index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     current_user != nil
   end
+
+  def require_user
+    unless logged_in?
+      flash[:error] = 'You must be logged in to perform that action'
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@
 # Articles controller for fetching, creating, destroying Articles
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :require_user, except: %i[show index]
 
   def index
     @articles = Article.recent
