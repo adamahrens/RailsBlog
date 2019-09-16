4fb85d76ea864a4142277de3060ce5d96303aba2
Adam Ahrens <adam.aahrens@gmail.com>
2019-09-15 21:27:49 -0500

    Handling unathorized errors to redirect to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user, :logged_in?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -16,5 +18,12 @@ class ApplicationController < ActionController::Base
       flash[:error] = 'You must be logged in to perform that action'
       redirect_to root_path
     end
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:error] = "You're not authorized"
+    redirect_to root_path
   end
 end
